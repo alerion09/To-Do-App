15bbf02e2b94625ef8337cceaeb9c7ca704e6c16
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-03-07 20:33:27 +0100

    Removed console logs and testing button.

--- src/App.js
+++ src/App.js
@@ -19,11 +19,9 @@ const App = ()=>{
     //UseEffect that is initializing only if activeUseEffect state is set to true
     useEffect(()=> {
         if (activeUseEffect) {
-            console.log('update')
             saveToLocalStorage()
         }
     },)
-    
     //Function checikng if tasks-list local storage item exist - if exist then pushing storaged data into tasksList useState
     //- if is not then creating empty array item in local storage. 
     const checkIsLocalTasksList = () => {
@@ -32,15 +30,10 @@ const App = ()=>{
         if (localTasksList) {
             setTasksList(JSON.parse(localTasksList))
             setCurrentId(JSON.parse(localCurrentId))
-            console.log('load local storage')
-            console.log(localTasksList)
         } 
         else {
             localStorage.setItem('tasks-list', '[]')
             localStorage.setItem('current-id', '1')
-            console.log('create local storage')
-            console.log(localStorage.getItem('tasks-list'))
-            console.log(localStorage.getItem('current-id'))
         }
     }
     //Function removing all objects from tasksList
@@ -52,8 +45,6 @@ const App = ()=>{
     const addTask = ()=>{
         const taskText = document.getElementById('task-text')
         if (taskText.value !== ''){
-            // currentId = currentId + 1
-
             const text = taskText.value
             setTasksList(oldList => [...oldList, {id: currentId,content: text}])
             setCurrentId(currentId + 1)
@@ -74,13 +65,11 @@ const App = ()=>{
     const saveToLocalStorage = () => {
         localStorage.setItem('tasks-list', JSON.stringify(tasksList))
         localStorage.setItem('current-id', JSON.stringify(currentId))
-        console.log(localStorage.getItem('tasks-list'))
     }
     return(
         <div className='container'>
             <ToDoList tasksList={tasksList} removeTask={removeTask} clearList={clearList} />
             <TaskInput addTask={addTask} />
-            <Button>click</Button>
         </div>
     )
 }
