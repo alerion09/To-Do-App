5fc18ad3cb7a237f72e6059f79cbadde4575f7ce
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-03-13 10:22:51 +0100

    Used useReducer hook instead useState.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useReducer} from 'react'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TaskInput from './TaskInput'
 import ToDoList from './ToDoList'
 
-// let currentId = 0
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_TASKS_LIST' :
+            const newList = action.payload
+            return {
+                ...state,
+                tasksList:newList
+            }
+        case 'UPDATE_TASKS_LIST' :
+            const newUpdatedList = [...state.tasksList, action.payload]
+            return {
+                ...state,
+                tasksList:newUpdatedList
+            }
+        case 'CLEAR_TASKS_LIST' :
+            return {
+               ...state,
+               tasksList:[],
+               activeUseEffect:true
+            }
+        case 'SET_CURRENT_ID' :
+            const newId = action.payload
+            return {
+                ...state,
+                currentId:newId
+            }
+        case 'INCREASE_CURRENT_ID' :
+            return {
+                ...state,
+                currentId: state.currentId + 1
+            }
+        case 'SET_TASK_TEXT' :
+            const newTaskText = action.payload
+            return {
+                ...state,
+                taskText:newTaskText
+            }
+        case 'TOGGLE_USE_EFFECT' :
+            const newUseEffectState = action.payload
+            return {
+                ...state,
+                activeUseEffect:newUseEffectState
+            }
+        case 'TOGGLE_INPUT_CORRECT' :
+            const newInputState = action.payload
+            return {
+                ...state,
+                isInputCorrect:newInputState
+            }
+        default:
+            throw new Error('WRONG TYPE OF DISPATCH')
+    }
+}
+const initialState = {
+    tasksList:[],
+    currentId:1,
+    taskText:'',
+    isInputCorrect:true,
+    activeUseEffect:false
+}
 const App = ()=>{
     
-    const [tasksList, setTasksList] = useState([])
-    const [currentId, setCurrentId] = useState(1)
-    const [activeUseEffect, setActiveUseEffect] = useState(false)
-    const [taskText, setTaskText] = useState('')
-    const [isInputCorrect, setIsInputCorrect] = useState(true)
+    const [state, dispatch] = useReducer(reducer, initialState)
     //UseEffect that is initializing only once  
     useEffect(() => {
         checkIsLocalTasksList()  
     },[])
     //UseEffect that is initializing only if activeUseEffect state is set to true
     useEffect(()=> {
-        if (activeUseEffect) {
+        if (state.activeUseEffect) {
             saveToLocalStorage()
         }
     },)
@@ -28,52 +83,58 @@ const App = ()=>{
         const localTasksList = localStorage.getItem('tasks-list')
         const localCurrentId = localStorage.getItem('current-id')
         if (localTasksList) {
-            setTasksList(JSON.parse(localTasksList))
-            setCurrentId(JSON.parse(localCurrentId))
+            const loadedTasksList = (JSON.parse(localTasksList))
+            const loadedCurrentId = (JSON.parse(localCurrentId))
+            console.log(loadedTasksList)
+            console.log(loadedCurrentId)
+            dispatch({type: 'SET_TASKS_LIST', payload: loadedTasksList})
+            dispatch({type: 'SET_CURRENT_ID', payload: loadedCurrentId})
         } 
         else {
             localStorage.setItem('tasks-list', '[]')
             localStorage.setItem('current-id', '1')
+            console.log('set items')
         }
     }
     //Function removing all objects from tasksList
     const clearList = () =>{
-        setTasksList([])
-        setActiveUseEffect(true)
+        dispatch({type: 'CLEAR_TASKS_LIST'})
     }
     //Set state of TaskText getting text from input
     const changeTaskText = (event) => {
-        setTaskText(event.target.value)
+        const newText = (event.target.value)
+        dispatch({type:'SET_TASK_TEXT', payload: newText})
     }
     //Function responsible for getting typed text, creating object and adding them into tasksList array
-    const addTask = ()=>{
-        const inputText = document.getElementById('task-text')
-        if (taskText !== ''){
-            setTasksList(oldList => [...oldList, {id: currentId, content: taskText}])
-            setCurrentId(currentId + 1)
-            setActiveUseEffect(true)
-            setIsInputCorrect(true)
-            setTaskText('')
-            inputText.value = ''
+    const addTask = (e)=>{
+        e.preventDefault()
+        if (state.taskText) {
+            const task = {id: state.currentId, content: state.taskText}
+            dispatch({type: 'UPDATE_TASKS_LIST', payload: task})
+            dispatch({type:'TOGGLE_USE_EFFECT', payload: true})
+            dispatch({type:'SET_TASK_TEXT', payload: ''})
+            dispatch({type:'TOGGLE_INPUT_CORRECT', payload: true})
+            dispatch({type:'INCREASE_CURRENT_ID'})
         }
         else {
-            setIsInputCorrect(false)
+            dispatch({type:'TOGGLE_INPUT_CORRECT', payload: false})
         }
     }
     //Function responsible for removing object from tasksList array
     const removeTask = (id) =>{
-        let filteredList = tasksList.filter((object) => object.id !== id)
-        setTasksList(filteredList)
-        setActiveUseEffect(true)
+        let filteredList = state.tasksList.filter((object) => object.id !== id)
+        console.log(filteredList)
+        dispatch({type: 'SET_TASKS_LIST', payload: filteredList})
+        dispatch({type:'TOGGLE_USE_EFFECT', payload:true})
     }
     const saveToLocalStorage = () => {
-        localStorage.setItem('tasks-list', JSON.stringify(tasksList))
-        localStorage.setItem('current-id', JSON.stringify(currentId))
+        localStorage.setItem('tasks-list', JSON.stringify(state.tasksList))
+        localStorage.setItem('current-id', JSON.stringify(state.currentId))
     }
     return(
         <div className='container'>
-            <ToDoList tasksList={tasksList} removeTask={removeTask} clearList={clearList} />
-            <TaskInput addTask={addTask} changeTaskText={changeTaskText} isInputCorrect={isInputCorrect}/>
+            <ToDoList tasksList={state.tasksList} removeTask={removeTask} clearList={clearList} />
+            <TaskInput taskText={state.taskText} addTask={addTask} changeTaskText={changeTaskText} isInputCorrect={state.isInputCorrect}/>
         </div>
     )
 }  
--- src/TaskInput.js
+++ src/TaskInput.js
@@ -2,18 +2,18 @@ import React from 'react'
 import {Button, Form} from 'react-bootstrap'
 
 const TaskInput = (props) => {
-    const {addTask, changeTaskText, isInputCorrect} = props
+    const {addTask, changeTaskText, isInputCorrect, taskText} = props
     return (
         <section>
-            <Form>
+            <Form onSubmit={addTask}>
                 <Form.Label>Enter your task</Form.Label>
                     <div className='flex-column-center'>
-                        <Form.Control id='task-text' placeholder='Something to do...' name='task-text' onChange={changeTaskText} 
-                        className={isInputCorrect ? 'form-control' : 'form-control is-invalid'} ></Form.Control>
+                        <Form.Control type='text' id='task-text' placeholder='Something to do...' name='task-text' onChange={changeTaskText} 
+                        value={taskText} className={isInputCorrect ? 'form-control' : 'form-control is-invalid'} ></Form.Control>
                         <Form.Control.Feedback type="invalid">
                             This field can not be empty.
                         </Form.Control.Feedback>
-                        <Button className='mt-3' onClick={addTask}>Add task</Button>
+                        <Button type='submit' className='mt-3'>Add task</Button>
                     </div>
             </Form> 
         </section>
